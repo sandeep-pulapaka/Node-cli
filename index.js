a943410548afacd7e241d78998b60ea9ce5d83f0
sandeep-pulapaka07 <sandeep.p6300@gmail.com>
2023-01-19 17:02:05 +0530

    node js cli

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const clearConsole = require('clear-any-console');
+
+// Clears the console.
+clearConsole();
+
+console.log(`
+sandeep pulapaka
+
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+Date :	19/01/2023
+`);
